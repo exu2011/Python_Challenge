95343bfb08f79be9e7141706461b0676f5b5f515
nu boule <eugeneNU@exus-mbp-2.lan>
2021-03-10 00:31:26 -0600

    update python

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,6 +4,9 @@ import collections as collect
 #define total votes
 total_votes = 0
 vote_percent = float
+winner = ""
+max_vote = 0
+
 pypoll_csv = "Resources/election_data.csv"
 with open(pypoll_csv) as csvfile:
 
@@ -19,23 +22,33 @@ print("Election Results")
 print("----------------------------")
 print(f"Total Votes: {total_votes}")
 print("----------------------------")
+
+# print candidate status
 for name in votes:
     percent_total = votes[name]/total_votes
     percentages = "{:.3%}".format(percent_total)
     print(f"{name}: {percentages}  ({votes[name]})")
 
+# print the last part
+print("----------------------------")    
+print(f"Winner: {votes.most_common(1)[0][0]}" )
 
+print("----------------------------")
 
- # Store the file path associated with the file (note the backslash may be OS specific)
-    file = open('analysis/pypoll.txt', "w")
-    file.write("Election Analysis\n")
-    file.write("----------------------------\n")
-    file.write("Total Votes: " + str(total_votes) + "\n")
-    file.write("----------------------------\n")
-    for name in votes:
-        percent_total = votes[name]/total_votes
-        percentages = "{:.3%}".format(percent_total)
-        file.write(name + ": " + str(percentages) + "("  + str(votes[name]) + ")\n")
-        
-    file.close()
+# Store the file path associated with the file (note the backslash may be OS specific)
+file = open('analysis/pypoll.txt', "w")
+file.write("Election Analysis\n")
+file.write("----------------------------\n")
+file.write("Total Votes: " + str(total_votes) + "\n")
+file.write("----------------------------\n")
+
+for name in votes:
+    percent_total = votes[name]/total_votes
+    percentages = "{:.3%}".format(percent_total)
+    file.write(name + ": " + str(percentages) + "("  + str(votes[name]) + ")\n")
+
+file.write("-----------------------------\n")
+file.write(f"Winner: {votes.most_common(1)[0][0]}\n")
+file.write("-----------------------------\n")
+file.close()
 
